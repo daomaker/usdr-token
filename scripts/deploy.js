bde84ec9031a7e1a00681979df45fe96db7b9335
ihux3 <ihux3x@gmail.com>
2021-08-30 17:45:43 +0200

    exact total supply

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 async function main() {
   const USDR = await ethers.getContractFactory("USDR");
-  const usdr = await USDR.deploy(ethers.utils.parseUnits("4500000", 18));
+  const usdr = await USDR.deploy(ethers.utils.parseUnits("7386245.347309997", 18));
   await usdr.deployed();
 
   console.log("USDR address:", usdr.address);
