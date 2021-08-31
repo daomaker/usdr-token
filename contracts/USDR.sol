28e25aaba6aefeda55003f2388cfde208757b404
ihux3 <ihux3x@gmail.com>
2021-08-31 13:44:37 +0200

    hardcoded total supply

--- contracts/USDR.sol
+++ contracts/USDR.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract USDR is ERC20 {
-    constructor(uint256 initialSupply) ERC20("USD Redeem", "USDR") {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20("USD Redeem", "USDR") {
+        _mint(msg.sender, 4761245347309997000000000);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 async function main() {
   const USDR = await ethers.getContractFactory("USDR");
-  const usdr = await USDR.deploy(ethers.utils.parseUnits("4886245.347309997", 18));
+  const usdr = await USDR.deploy();
   await usdr.deployed();
 
   console.log("USDR address:", usdr.address);
