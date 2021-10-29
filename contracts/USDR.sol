dac8ddd8c5e0b83cf10348e5929aa2f8a32b5c87
iHux3 <35241224+iHux3@users.noreply.github.com>
2021-10-29 12:16:45 +0200

    rounded total supply to 2 decimal places

--- contracts/USDR.sol
+++ contracts/USDR.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract USDR is ERC20 {
     constructor() ERC20("USD Redeem", "USDR") {
-        _mint(msg.sender, 4761245347309997000000000);
+        _mint(msg.sender, 4761245380000000000000000);
     }
 }
